77b35fc4e0cc6528fbadb6c987dea1c6ffee5496
SkMdSufiyan <107339274+SkMdSufiyan@users.noreply.github.com>
2023-01-22 08:36:20 +0530

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,6 +5,9 @@ export default function Dashboard(){
     const navigate=useNavigate();
     return(
         <Container style={{marginTop:"20px"}}>
+        
+        <p className="netlify-suggestion-p">Kindly <b>DO NOT REFRESH</b> this netlify site when you are in the page other than home (dashboard) page. If you refresh on page other than home page, then it will give "page not found" (problem with netlify deployment). </p>
+            
             <br />
             <h6>Dashboard</h6>
             {/* Button for creating a new user */}
